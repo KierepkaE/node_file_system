668f2cba68c7c15824dcb128445feab0d1ed02b2
kierepkae <kierepkaedyta@gmail.com>
2019-10-14 11:51:19 +0100

    change newNames.txt to names.txt  with sync method

--- index.js
+++ index.js
@@ -3,9 +3,12 @@ fs.access('./names.txt', fs.constants.W_OK, (err) => {
   console.log(err);
 });
 
-fs.rename('names.txt', 'newNames.txt', (err) => {
-  if (err) throw err;
-  if (err) console.log(err);
-  console.log('Name changed.')
+// fs.rename('names.txt', 'newNames.txt', (err) => {
+//   if (err) throw err;
+//   if (err) console.log(err);
+//   console.log('Name changed.')
 
-})
+// })
+
+
+fs.renameSync('newNames.txt', 'names.txt')
