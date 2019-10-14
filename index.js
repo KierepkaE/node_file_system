c09351d7b59e181c80a7cc6d54442c2113097e1b
kierepkae <kierepkaedyta@gmail.com>
2019-10-14 11:56:49 +0100

    user readdirSync to get list of all files in the directory

--- index.js
+++ index.js
@@ -1,18 +1,2 @@
 const fs = require('fs');
-fs.access('./names.txt', fs.constants.W_OK, (err) => {
-  console.log(err);
-});
-
-// fs.rename('names.txt', 'newNames.txt', (err) => {
-//   if (err) throw err;
-//   if (err) console.log(err);
-//   console.log('Name changed.')
-
-// })
-
-
-try {
-  fs.renameSync('names.txt', 'users.txt');
-} catch (err) {
-  console.log(err);
-}
+console.log(fs.readdirSync('./'));
