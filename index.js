2a51619f722b5e35fc9c5fae79359e5cf5abbc1f
kierepkae <kierepkaedyta@gmail.com>
2019-10-14 11:49:45 +0100

    rename names.txt to newNames.txt

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
 const fs = require('fs');
-fs.access('./names.txt', (err) => {
+fs.access('./names.txt', fs.constants.W_OK, (err) => {
   console.log(err);
 });
+
+fs.rename('names.txt', 'newNames.txt', (err) => {
+  if (err) throw err;
+  if (err) console.log(err);
+  console.log('Name changed.')
+
+})
